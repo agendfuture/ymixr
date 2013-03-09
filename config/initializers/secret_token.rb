5d8088255d4400b72fdb0917df73811c63e8653d
Johannes Koch <jkoch@jkoch-530U3BI-530U4BI-530U4BH.(none)>
2013-03-09 19:17:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoumixrOR::Application.config.secret_token = 'e36b375630037dda9cd17bf0d1122eecf1be2ace183c717422e1965f3c7fc4761a141c3618ee26f0e092c32202c85bb623ea766e861dc0f26de1ac3cd11d0821'
