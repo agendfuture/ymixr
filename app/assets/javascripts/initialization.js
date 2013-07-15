eaef581cd5a90cb8c9de1cb79296ed94c0e4e809
Johannes Koch <jkoch.contact@gmail.com>
2013-07-15 21:41:21 +0200

    bugfix: sidebar serchform doesnt work

--- app/assets/javascripts/initialization.js
+++ app/assets/javascripts/initialization.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
                 .delegate(".playlist-small .close", "click", playlist.removeElement)
                 .delegate(".player .btn-forward", "click", $.proxy(player.next, player));
       
-      $("#search-form").submit(submitSearch);
+      $(".navbar-form").submit(submitSearch);
 
       $(".search-result-list, .playlist-small" ).sortable({
         connectWith: ".connectedSortable",
