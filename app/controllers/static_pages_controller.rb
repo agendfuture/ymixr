428eccef3a6137f8b8700b56ecdaf0972f0c58d5
Johannes Koch <jkoch.contact@gmail.com>
2013-08-30 18:04:41 +0200

    correct query

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,7 +9,7 @@ class StaticPagesController < ApplicationController
 	if !session[:playlist].nil?
         @playlist_entries = Song.all(:joins => :playlist_entries, 
                     :conditions => {:playlist_entries => {:playlist_id => session[:playlist].id}}, 
-                    :select => "songs.*, playlist_entries.'order', playlist_entries.id as playlist_entry_id")
+                    :select => 'songs.*, playlist_entries."order", playlist_entries.id as playlist_entry_id')
     end
 
 	render template: "layouts/index"
