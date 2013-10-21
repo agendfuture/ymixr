f9f9e660f801596a9f073a16bef8114ccb770475
Johannes Koch <jkoch.contact@gmail.com>
2013-10-21 22:34:08 +0200

    some docs; refactor authorization as a controller filter

--- app/controllers/playlist_entries_controller.rb
+++ app/controllers/playlist_entries_controller.rb
@@ -1,60 +1,65 @@
 class PlaylistEntriesController < ApplicationController
- def create
-  if choosed_own_playlist      
-      params[:id] = (params[:id] == session[:playlist].id)? params[:id] : session[:playlist].id
+  before_filter :authorize
 
-      song = Song.findOrCreate(params[:sid], params[:title], params[:artist])
-   
-      entry = PlaylistEntry.create(playlist_id: params[:id], song_id: song.id)
-
-      if !entry.save
-        flash[:notice] = "Error while creating a new playlist entry!"
-      end
-
-    respond_to do |format|
-      format.html {render :text => "{\"id\" : #{entry.id}}"}
-      format.js 
-    end      
-  else 
-    redirect_to run_path, notice: 'You have to login to create or change playlists!'      
+  def authorize
+    if !choosed_own_playlist
+      redirect_to run_path, notice: 'You have to login to create or change playlists!'      
+    end
   end
+
+  # POST /playlists/:playlist_id/playlist_entries/:playlist_entry_id/add      Deprecated !!
+  # POST /playlists/:playlist_id/playlist_entries/:playlist_entry_id/add.js
+
+  # POST /playlists/:playlist_id/playlist_entries
+  # POST /playlists/:playlist_id/playlist_entries.js
+
+  # GET  /playlists/addSong/:sid
+  # GET  /playlists/addSong/:sid.js
+ def create    
+  params[:id] = (params[:id] == session[:playlist].id)? params[:id] : session[:playlist].id
+
+  song = Song.findOrCreate(params[:sid], params[:title], params[:artist])
+
+  entry = PlaylistEntry.create(playlist_id: params[:id], song_id: song.id)
+
+  if !entry.save
+    flash[:notice] = "Error while creating a new playlist entry!"
+  end
+
+  respond_to do |format|
+    format.html {render :text => "{\"id\" : #{entry.id}}"}
+    format.js 
+  end      
  end
 
+ # DELETE /playlists/:playlist_id/playlist_entries/:id
+ # DELETE /playlists/:playlist_id/playlist_entries/:playlist_entry_id/remove Deprecated!!!
+ # GET    /playlists/removeSong/:playlist_entry_id
  def destroy
-   if choosed_own_playlist
-      if !(entry = PlaylistEntry.find(params[:playlist_entry_id])).nil?
-        entry.destroy      
-      else
-        flash[:notice] = "Cannot find any Playlist Entry!"
-      end
-      redirect_to :back
-    else 
-      redirect_to run_path, notice: 'You have to login to remove songs from a playlist!'
-    end
+  if !(entry = PlaylistEntry.find(params[:playlist_entry_id])).nil?
+    entry.destroy      
+  else
+    flash[:notice] = "Cannot find any Playlist Entry!"
+  end
+  redirect_to :back
  end
 
- # GET playlist_entry_id, next_playlist_entry_id
- 
+ # GET /playlists/reorder/:playlist_entry_id/:next_playlist_entry_id
+ # GET /playlists/reorder/:playlist_entry_id
  def reorder
-  if choosed_own_playlist
-    
-    playlist = PlaylistEntry.where(playlist_id: session[:playlist].id)
+  playlist = PlaylistEntry.where(playlist_id: session[:playlist].id)
+  entry = playlist.find(params[:playlist_entry_id])
 
-    entry = playlist.find(params[:playlist_entry_id])
-
-    if !params[:next_playlist_entry_id].nil?
-      next_entry = playlist.find(params[:next_playlist_entry_id])
-      if !next_entry.previous.nil?
-        entry.append_to(next_entry.previous) 
-      else
-        entry.prepend
-      end     
+  if !params[:next_playlist_entry_id].nil?
+    next_entry = playlist.find(params[:next_playlist_entry_id])
+    if !next_entry.previous.nil?
+      entry.append_to(next_entry.previous) 
     else
-      entry.append_to(playlist.last_in_order)
-    end
-    render :nothing => true
+      entry.prepend
+    end     
   else
-    redirect_to run_path, notice: 'You have to login to reorder songs in a playlist!'
+    entry.append_to(playlist.last_in_order)
   end
+  render :nothing => true
  end
 end
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,4 +1,14 @@
 class PlaylistsController < ApplicationController
+  before_filter :authorize, :only => [:new, :edit, :update, :create, :destroy]
+
+  def authorize
+    if !logged_in
+      respond_to do |format|
+        format.html {redirect_to run_path, notice: 'You have to login to edit playlists!'}
+      end
+    end
+  end
+
   # GET /playlists
   # GET /playlists.json
   def index
@@ -33,59 +43,41 @@ class PlaylistsController < ApplicationController
   # GET /playlists/new
   # GET /playlists/new.json
   def new
-    if logged_in
-      @playlist = Playlist.new
+    @playlist = Playlist.new
 
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render json: @playlist }
-      end
-    else 
-      respond_to do |format|
-        format.html {redirect_to run_path, notice: 'You have to login to create playlists!'}
-      end
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @playlist }
     end
   end
 
   # GET /playlists/1/edit
   def edit
-    if logged_in
-      @playlist = Playlist.find(params[:id])
-      @username = User.find(@playlist.creator)
-    else 
-      respond_to do |format|
-        format.html {redirect_to run_path, notice: 'You have to login to edit playlists!'}
-      end
-    end
+    @playlist = Playlist.find(params[:id])
+    @username = User.find(@playlist.creator)
   end
 
   # POST /playlists
   # POST /playlists.json
   def create
-    if logged_in
-      if !params[:playlist][:title].blank?
-        @playlist = Playlist.new(:title => params[:playlist][:title], :creator => current_user.id)
-        @playlist.update_attributes(params[:playlist])
+    if !params[:playlist][:title].blank?
+      @playlist = Playlist.new(:title => params[:playlist][:title], :creator => current_user.id)
+      @playlist.update_attributes(params[:playlist])
 
-        respond_to do |format|
-          if @playlist.save
-            session[:playlist] = @playlist
+      respond_to do |format|
+        if @playlist.save
+          session[:playlist] = @playlist
 
-            format.html { redirect_to @playlist, notice: 'Playlist was successfully created.' }
-            format.json { render json: @playlist, status: :created, location: @playlist }
-          else
-            format.html { render action: "new" }
-            format.json { render json: @playlist.errors, status: :unprocessable_entity }
-          end
-        end
-      else
-        respond_to do |format|
-          format.html {redirect_to run_path, notice: 'Cannot create a playlist without a name!'}
+          format.html { redirect_to @playlist, notice: 'Playlist was successfully created.' }
+          format.json { render json: @playlist, status: :created, location: @playlist }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @playlist.errors, status: :unprocessable_entity }
         end
       end
-    else 
+    else
       respond_to do |format|
-        format.html {redirect_to run_path, notice: 'You have to login to create playlists!'}
+        format.html {redirect_to run_path, notice: 'Cannot create a playlist without a name!'}
       end
     end
   end
@@ -93,21 +85,15 @@ class PlaylistsController < ApplicationController
   # PUT /playlists/1
   # PUT /playlists/1.json
   def update
-    if logged_in
-      @playlist = Playlist.find(params[:id])
+    @playlist = Playlist.find(params[:id])
 
-      respond_to do |format|
-        if @playlist.update_attributes(params[:playlist])
-          format.html { redirect_to @playlist, notice: 'Playlist was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render action: "edit" }
-          format.json { render json: @playlist.errors, status: :unprocessable_entity }
-        end
-      end
-    else 
-      respond_to do |format|
-        format.html {redirect_to run_path, notice: 'You have to login to create playlists!'}
+    respond_to do |format|
+      if @playlist.update_attributes(params[:playlist])
+        format.html { redirect_to @playlist, notice: 'Playlist was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @playlist.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -115,18 +101,12 @@ class PlaylistsController < ApplicationController
   # DELETE /playlists/1
   # DELETE /playlists/1.json
   def destroy
-    if logged_in
-      @playlist = Playlist.find(params[:id])
-      @playlist.destroy
+    @playlist = Playlist.find(params[:id])
+    @playlist.destroy
 
-      respond_to do |format|
-        format.html { redirect_to playlists_url }
-        format.json { head :no_content }
-      end
-    else 
-      respond_to do |format|
-        format.html {redirect_to run_path, notice: 'You have to login to create playlists!'}
-      end
+    respond_to do |format|
+      format.html { redirect_to playlists_url }
+      format.json { head :no_content }
     end
   end
 
